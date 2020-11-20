6633204918e681734a35a549643daabcee35337a
Dmitriy Suchkov <dsuchkov@jet.su>
2020-11-20 12:38:28 +0300

    fix: move connection  to context manager

--- follow_chat.py
+++ follow_chat.py
@@ -1,8 +1,10 @@
 import asyncio
 import sys
+import socket
 
 import click
 from loguru import logger
+from contextlib import asynccontextmanager
 
 
 def get_chat_logger(history_path: str, log_level: str) -> logger:
@@ -32,6 +34,40 @@ def get_chat_logger(history_path: str, log_level: str) -> logger:
     return chat_logger
 
 
+@asynccontextmanager
+async def chat_connection(
+    host: str,
+    port: int,
+    db: int = 0,
+    connection_timeout: int = 5,
+) -> (asyncio.StreamReader, asyncio.StreamWriter):
+
+    try:
+        logger.debug(
+            "trying to connect to chat", extra={"host": host, "port": port, "db": db}
+        )
+        reader, writer = await asyncio.wait_for(
+            asyncio.open_connection(host, port), connection_timeout
+        )
+        logger.debug(
+            "redis connected successfully",
+            extra={"reader": reader.__repr__(), "writer": writer.__repr__()},
+        )
+
+    except (ConnectionRefusedError, asyncio.exceptions.TimeoutError, socket.gaierror):
+        logger.error(
+            "cannot connect to chat server",
+            extra={"host": host, "port": port, "db": db},
+        )
+        raise
+
+    try:
+        yield reader, writer
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+
 @click.command()
 @click.option(
     "-h",
@@ -53,24 +89,21 @@ def get_chat_logger(history_path: str, log_level: str) -> logger:
 @click.option("-l", "--log_level", envvar="LOG_LEVEL", default="INFO")
 def main(host, port, history_path, log_level):
     asyncio.run(
-        follow_chat(host=host, port=port, chat_logger=get_chat_logger(log_level=log_level, history_path=history_path))
+        follow_chat(
+            host=host,
+            port=port,
+            chat_logger=get_chat_logger(log_level=log_level, history_path=history_path),
+        )
     )
 
 
 async def follow_chat(host, port, chat_logger, connection_timeout=5):
-    try:
-        reader, writer = await asyncio.wait_for(
-            asyncio.open_connection(host, port), connection_timeout
-        )
-    except asyncio.TimeoutError:
-        logger.error("can`t connect to chat server")
-        return
-
-    while True:
-        # по поводу отказоустойчивасти: выключил сеть , подождал минуту, включил - все само заработало. Т.е. доп. обработки не требуется
-        data = await reader.readline()
-        # по поводу решения выводить сообщения через логгер я не уверен. Интересно твое мнение
-        chat_logger.info(data.decode(encoding="utf8"))
+    async with chat_connection(host, port) as (reader, writer):
+        while True:
+            # по поводу отказоустойчивасти: выключил сеть , подождал минуту, включил - все само заработало. Т.е. доп. обработки не требуется
+            data = await reader.readline()
+            # по поводу решения выводить сообщения через логгер я не уверен. Интересно твое мнение
+            chat_logger.info(data.decode(encoding="utf8"))
 
 
 if __name__ == "__main__":
