f3afa77a51d2e0016beb8cad45f9e2792afa5fdc
Dmitriy Suchkov <dsuchkov@jet.su>
2020-11-20 19:32:05 +0300

    fix: rewrite printing history and writing to file

--- follow_chat.py
+++ follow_chat.py
@@ -2,38 +2,17 @@ import asyncio
 import sys
 import socket
 from anyio import create_task_group, move_on_after, sleep, run, fail_after
+from datetime import datetime
+
+import aiofiles
 
 import click
 from loguru import logger
 from contextlib import asynccontextmanager
 
 
-def get_chat_logger(history_path: str, log_level: str) -> logger:
-    chat_logger = logger.bind(name="chat_logger")
-    chat_logger.configure(
-        handlers=[
-            {
-                "sink": history_path,
-                "format": "<yellow>[{time:YY-MM-DD HH:mm}] {message}</yellow>",
-                "level": "INFO",
-                "filter": lambda record: record["extra"].get("name") == "chat_logger",
-            },
-            {
-                "sink": sys.stdout,
-                "format": "<yellow>[{time:YY-MM-DD HH:mm}] {message}</yellow>",
-                "level": "INFO",
-                "filter": lambda record: record["extra"].get("name") == "chat_logger",
-            },
-            {
-                "sink": sys.stdout,
-                "format": "<level>{level: <6}</level>:<cyan>{name:<6}</cyan>:<level>{message:>32}</level>:{extra}",
-                "level": log_level,
-                "filter": lambda record: not record["extra"].get("name"),
-            },
-        ]
-    )
-    return chat_logger
-
+class ConnecChatException(Exception):
+    pass
 
 @asynccontextmanager
 async def chat_connection(
@@ -56,13 +35,10 @@ async def chat_connection(
     except (
         ConnectionRefusedError,
         socket.gaierror,
+        asyncio.exceptions.CancelledError,
         TimeoutError,
     ):
-        logger.error(
-            "cannot connect to chat server",
-            extra={"host": host, "port": port, "db": db},
-        )
-        exit(0)
+        raise ConnecChatException()
 
     try:
         yield reader, writer
@@ -70,7 +46,6 @@ async def chat_connection(
         writer.close()
         await writer.wait_closed()
 
-    #
 
 
 @click.command()
@@ -81,7 +56,7 @@ async def chat_connection(
     envvar="CHAT_HOST",
     help="chat hostname",
 )
-@click.option("--port", envvar="CHAT_INCOME_MSG_PORT", default=5001)
+@click.option("--port", envvar="CHAT_INCOME_MSG_PORT", default=5000)
 @click.option(
     "--history ",
     "-H",
@@ -93,22 +68,39 @@ async def chat_connection(
 )
 @click.option("-l", "--log_level", envvar="LOG_LEVEL", default="INFO")
 def main(host, port, history_path, log_level):
-    asyncio.run(
-        follow_chat(
-            host=host,
-            port=port,
-            chat_logger=get_chat_logger(log_level=log_level, history_path=history_path),
-        )
+    logger.configure(
+        handlers=[
+            dict(
+                sink=sys.stdout,
+                format="<level>{level: <6}</level>:<cyan>{name:<6}</cyan>:<level>{message:>32}</level>:{extra}",
+                level=log_level,
+            ),
+        ]
     )
+    try:
+        asyncio.run(
+            follow_chat(
+                host=host,
+                port=port,
+                history_path=history_path,
+            )
+        )
+    except ConnecChatException as e:
+        logger.error('can`t connect to chat', extra={'host': host, 'port': port})
+    except FileNotFoundError as e:
+        logger.error('con`t add record to history file', extra={'file': history_path})
 
 
-async def follow_chat(host, port, chat_logger, connection_timeout=5):
-    async with chat_connection(host, port) as (reader, writer):
-        while True:
-            # по поводу отказоустойчивасти: выключил сеть , подождал минуту, включил - все само заработало. Т.е. доп. обработки не требуется
-            data = await reader.readline()
-            # по поводу решения выводить сообщения через логгер я не уверен. Интересно твое мнение
-            chat_logger.info(data.decode(encoding="utf8"))
+async def follow_chat(host, port, history_path):
+
+        async with chat_connection(host, port) as (reader, writer):
+            while True:
+                # по поводу отказоустойчивасти: выключил сеть , подождал минуту, включил - все само заработало. Т.е. доп. обработки не требуется
+                income_message_text = (await reader.readline()).decode(encoding="utf8")
+                message_info = f"[{datetime.now().strftime('%d.%m.%y %H:%M')}] {income_message_text}"
+                print(message_info)
+                async with aiofiles.open(history_path, 'a') as file:
+                    await file.write(message_info)
 
 
 if __name__ == "__main__":
